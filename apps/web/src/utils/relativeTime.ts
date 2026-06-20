c864698d9a6a709c0bac42036037ea1f94a7f050
SomeoneUnlicensed <kompotomanya@gmail.com>
2026-06-20 23:31:05 +0300

    fix: краш поиска, доработка страницы /explore

--- apps/web/src/utils/relativeTime.ts
+++ apps/web/src/utils/relativeTime.ts
@@ -1,9 +1,11 @@
 import { formatDistanceToNow, formatDistanceToNowStrict } from 'date-fns';
+import { ru } from 'date-fns/locale';
 
 export const getRelativeTimeStrict = (date: Date | string) => {
   if (typeof date === 'string') date = new Date(date);
   return formatDistanceToNowStrict(date, {
     addSuffix: true,
+    locale: ru,
   });
 };
 
@@ -11,5 +13,6 @@ export const getRelativeTime = (date: Date | string) => {
   if (typeof date === 'string') date = new Date(date);
   return formatDistanceToNow(date, {
     addSuffix: true,
+    locale: ru,
   });
 };
