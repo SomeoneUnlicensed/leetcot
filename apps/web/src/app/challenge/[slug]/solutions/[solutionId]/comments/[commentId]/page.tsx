84d23df39021f9f36e88fecc7eb5ea17230afdbf
SomeoneUnlicensed <kompotomanya@gmail.com>
2026-06-20 12:53:04 +0300

    chore: временно скрыт блок комментариев

--- apps/web/src/app/challenge/[slug]/solutions/[solutionId]/comments/[commentId]/page.tsx
+++ apps/web/src/app/challenge/[slug]/solutions/[solutionId]/comments/[commentId]/page.tsx
@@ -1,7 +1,5 @@
 import { auth } from '~/server/auth';
 import { SolutionDetails } from '~/app/challenge/[slug]/solutions/_components/solution-detail';
-import { Comments } from '~/app/challenge/_components/comments';
-import { getPreselectedSolutionCommentMetadata } from '~/app/challenge/_components/comments/getCommentRouteData';
 import { getSolutionIdRouteData } from '../../getSolutionIdRouteData';
 
 interface SolutionPageCommentsProps {
@@ -15,25 +13,14 @@ interface SolutionPageCommentsProps {
 export default async function SolutionPageComments(props: SolutionPageCommentsProps) {
   const params = await props.params;
 
-  const { solutionId, commentId, slug } = params;
+  const { solutionId, slug } = params;
 
   const session = await auth();
   const solution = await getSolutionIdRouteData(slug, solutionId, session);
-  const preselectedCommentMetadata = await getPreselectedSolutionCommentMetadata(
-    Number(solutionId),
-    Number(commentId),
-    Number(solution.challengeId),
-  );
 
   return (
     <div className="relative h-full">
       <SolutionDetails solution={solution} />
-      <Comments
-        root={solution}
-        type="SOLUTION"
-        preselectedCommentMetadata={preselectedCommentMetadata}
-        expanded
-      />
     </div>
   );
 }
